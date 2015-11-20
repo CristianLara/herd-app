a9a030c320fbfee6e9823e971f63f9c3eacba074
Neven Wang-Tomic <nevenwt@gmail.com>
2015-11-19 18:23:08 -0800

    Added a back button to Phi Psi Volcano Party page..... and deez nuts.

--- herd/ViewController.swift
+++ herd/ViewController.swift
@@ -17,9 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("HERDDDD")
-        
+        // Do any additional setup after loading the view, typically from a nib.        
     }
 
     override func didReceiveMemoryWarning() {
